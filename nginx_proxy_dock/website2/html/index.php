b1488f4cae43bbbdd6df95b743ddd2ebcb41b55c
WorWh3r3 <worzwhere@gmail.com>
2021-02-04 11:20:19 +0700

    Update and rename index_edit.php to index.php

--- nginx_proxy_dock/website2/html/index.php
+++ nginx_proxy_dock/website2/html/index.php
@@ -5,7 +5,7 @@
 
    $dbhandle = mysqli_connect($servername,$username,$password);
    $selected = mysqli_select_db($dbhandle, "titanic");
-   echo "Hello : from service.lab20.cpsudevops.com<br>";
+   echo "Hello : from PHP Playground node 2<br>";
    echo "Connected database server<br>";
    echo "Selected database";
 ?>
