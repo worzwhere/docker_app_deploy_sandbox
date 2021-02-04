9ed11cd00ac494414fc8bc08873d274da00f89d4
WorWh3r3 <worzwhere@gmail.com>
2021-02-04 11:20:59 +0700

    Update dockerNetworkBuild.sh

--- nginx_proxy_dock/dockerNetworkBuild.sh
+++ nginx_proxy_dock/dockerNetworkBuild.sh
@@ -1,4 +1,6 @@
 docker network rm webproxy
 docker network rm web_network
+docker network rm web_network2
 docker network create webproxy
 docker network create web_network
+docker network create web_network2
